4b66fce5eaccfb2da62dd319a7acbd48bb53736c
yashvij19 <yashvijay049@gmail.com>
2025-05-19 10:07:54 +0530

    added ta code and category code

--- src/app/services/tas/ta-service.service.ts
+++ src/app/services/tas/ta-service.service.ts
@@ -1,5 +1,48 @@
-import { HttpClient } from '@angular/common/http';
+// import { HttpClient, HttpHeaders } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class TaServiceService {
+//   private apiBaseUrl = 'https://localhost:7264';
+
+//   constructor(private http: HttpClient) {}
+
+//   getTaById(taId: number) {
+//     const url = `${this.apiBaseUrl}/api/Ta/${taId}`;
+//     return this.http.get<{ status: boolean }>(url);
+//   }
+
+//   updateTa(taId: number, taData: { status: boolean }) {
+//     const url = `${this.apiBaseUrl}/api/Ta/${taId}`;
+//     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+//     return this.http.put(url, taData, { headers });
+//   }
+
+//   addTa(taData: any) {
+//     const url = `${this.apiBaseUrl}/api/Ta`;
+//     return this.http.post(url, taData);
+//   }
+
+//   viewTas() {
+//     const url = `${this.apiBaseUrl}/api/Ta`;
+//     return this.http.get(url);
+//   }
+// }
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+interface Ta {
+  taId: number;
+  adminId: number;
+  name: string;
+  email: string;
+  phone: number;
+  status: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -7,9 +50,26 @@ import { Injectable } from '@angular/core';
 export class TaServiceService {
   private apiBaseUrl = 'https://localhost:7264';
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) {}
 
-  
-    
+  viewTas() {
+    const url = `${this.apiBaseUrl}/api/Ta`;
+    return this.http.get<Ta[]>(url);
+  }
+
+  addTa(taData: any) {
+    const url = `${this.apiBaseUrl}/api/Ta`;
+    return this.http.post(url, taData);
+  }
+
+  getTaById(taId: number) {
+    const url = `${this.apiBaseUrl}/api/Ta/${taId}`;
+    return this.http.get<Ta>(url);
+  }
+
+  updateTa(taId: number, taData: { status: boolean }) {
+    const url = `${this.apiBaseUrl}/api/Ta/${taId}`;
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.put(url, taData, { headers });
   }
 }
